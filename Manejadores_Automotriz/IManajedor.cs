ca1bedd2b8a8dc052d319f3c4ad9a076a338626a
Ariel <ari.reyes195@gmail.com>
2022-10-09 18:05:03 -0500

    Se agregaron las clases manajadores de cada entidad

---
+++ Manejadores_Automotriz/IManajedor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Manejadores_Automotriz
+{
+    interface IManajedor
+    {
+        void Borrar(dynamic Entidad);
+        void Guardar(dynamic Entidad);
+        void Mostrar(DataGridView tabla);
+    }
+}
--- Manejadores_Automotriz/Manejador_Taller.cs
+++ Manejadores_Automotriz/Manejador_Taller.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Manejadores_Automotriz
 {
-    public class Class1
+    class Manejador_Taller
     {
     }
 }
